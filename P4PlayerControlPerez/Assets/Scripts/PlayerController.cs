63e0bca1016ff8b180f993d80debb591834065b5
737104-glitch <737104@student.dusd.net>
2026-02-06 11:44:28 -0800

    Car
    fixed car and added pyramid and car obstacles

--- P4PlayerControlPerez/Assets/Scripts/PlayerController.cs
+++ P4PlayerControlPerez/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +17,10 @@ public class PlayerController : MonoBehaviour
     {
         //We'll move the vehicle forward
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
     }
 }
